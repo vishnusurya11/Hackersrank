b629b1da395b00d21b4c55a17494ffd3f116ee4b
Vishnu Surya Reddy Nandi <vishnusurya11@gmail.com>
2018-01-21 13:08:05 -0800

    Create Day_1.py

---
+++ 30 days of code/Day_1.py
@@ -0,0 +1,18 @@
+
+# Declare second integer, double, and String variables.
+
+# Read and save an integer, double, and String to your variables.
+x= input();
+y= input();
+z= input();
+a=int(x);
+b=float(y);
+
+
+# Print the sum of both integer variables on a new line.
+print(a+i)
+# Print the sum of the double variables on a new line.
+print(b+d)
+# Concatenate and print the String variables on a new line
+print(s+z)
+# The 's' variable above should be printed first.
