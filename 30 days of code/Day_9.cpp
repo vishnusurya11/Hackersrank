46d06c672ab257e4ba6a2529fff786116de33301
Vishnu Surya Reddy Nandi <vishnusurya11@gmail.com>
2018-01-21 14:36:24 -0800

    Create Day_9.cpp

---
+++ 30 days of code/Day_9.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int factorial(int n) {
+    // Complete this function
+    if( n ==1 ){
+        return 1;
+    }
+    else
+        return n*factorial(n-1);
+}
+
+int main() {
+    int n;
+    cin >> n;
+    int result = factorial(n);
+    cout << result << endl;
+    return 0;
+}
